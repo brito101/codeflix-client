5dd4eab4b28e53a5fae311bc3d7ba0fe64262112
brito101 <rodrigobrito101@hotmail.com>
2025-04-02 19:48:42 -0300

    feat: add headers components

--- src/app/components/Banner.tsx
+++ src/app/components/Banner.tsx
@@ -10,14 +10,6 @@ export function Banner() {
     <div className="mb-10 lg:mb-20">
       <div className="flex flex-col space-y-4 py-16 md:space-y-4 lg:h-[65vh] lg:justify-end lg:pb-12">
         <div className="absolute left-0 top-0 -z-10 flex h-[95vh] w-screen flex-col bg-black">
-          {/* <video
-            autoPlay
-            loop
-            muted
-            className='z-20 hidden h-full w-full object-cover opacity-50 transition duration-1000 ease-in-out lg:block'
-            poster=
-            src=
-          /> */}
           <Image
             src="/banner.png"
             alt="banner"
