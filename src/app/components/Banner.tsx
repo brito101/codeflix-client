96efbc755611bfa4cc76f1f8b111852d5e4e7f5e
brito101 <rodrigobrito101@hotmail.com>
2025-04-01 22:18:57 -0300

    mod: initi banner

---
+++ src/app/components/Banner.tsx
@@ -0,0 +1,9 @@
+export function Banner() {
+  return (
+    <div className="mb-10 lg:mb-20">
+      <div className="flex flex-col space-y-4 py-16 md:space-y-4 lg:h-[65vh] lg:justify-end lg:pb-12">
+        <div className="absolute left-0 top-0 -z-10 flex h-[95vh] w-screen flex-col bg-black"></div>
+      </div>
+    </div>
+  );
+}
