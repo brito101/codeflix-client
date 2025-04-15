8d8d91da05c48ce00e2cfb33f3675f2f30346923
brito101 <rodrigobrito101@hotmail.com>
2025-04-15 10:16:28 -0300

    mod: add env.local

--- src/app/config/index.ts
+++ src/app/config/index.ts
@@ -1,4 +1,4 @@
 export const config = {
-  API_URL: "https://codeflix-api.vercel.app",
+  API_URL: process.env.API_URL || "https://codeflix-api.vercel.app",
   featuredIds: ["101", "102", "103", "104", "105", "106"],
 };
